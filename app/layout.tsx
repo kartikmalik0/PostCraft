36e183dab5eeeb8d75e01f6bb333c9971897d430
Kartik <kartik20044@gmail.com>
2025-05-24 09:22:14 +0530

    added analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,13 +3,13 @@ import "@/app/globals.css"
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import { ThemeProvider } from "@/components/theme-provider"
-
+import { Analytics } from "@vercel/analytics/next"
 const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
   title: "Social Media Post Generator",
   description: "Create and download custom LinkedIn or X (Twitter) posts",
-    generator: 'v0.dev'
+  generator: 'v0.dev'
 }
 
 export default function RootLayout({
@@ -22,6 +22,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
           {children}
+          <Analytics />
         </ThemeProvider>
       </body>
     </html>
